a8bd9ed5b6f2c37a105a5d4043dbb843b13959cf
El-Nath <gamakireta@gmail.com>
2015-09-21 04:07:28 +0700

    thermal : tune down core temp to default
    Safety over performance, use manufacturer default setting during boot-up
    
    Signed-off-by: El-Nath <gamakireta@gmail.com>

--- drivers/thermal/intelli/msm_thermal.c
+++ drivers/thermal/intelli/msm_thermal.c
@@ -56,11 +56,11 @@ static struct msm_thermal_data msm_thermal_info;
 static struct msm_thermal_data_intelli msm_thermal_info_local = {
 	.sensor_id = 5,
 	.poll_ms = DEFAULT_POLLING_MS,
-	.limit_temp_degC = 70,
+	.limit_temp_degC = 60,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 	.freq_control_mask = 0xf,
-	.core_limit_temp_degC = 80,
+	.core_limit_temp_degC = 75,
 	.core_temp_hysteresis_degC = 10,
 	.core_control_mask = 0xe,
 };
